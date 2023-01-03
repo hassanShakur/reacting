def58bcc1cc803c8f1717a2736826b762be3b69a
hassanShakur <hass.shakurhassan@gmail.com>
2023-01-03 18:36:30 +0300

    Component tenets

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Creating component
+// Component tenets : Nesting, Reusability & Configuration
 
 const App = () => {
-  return <div>Hello there</div>;
+  return <div style={{color: 'red'}}>Hello there</div>;
 };
 
 // Rendering the component
