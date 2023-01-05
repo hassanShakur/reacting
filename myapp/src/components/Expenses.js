7706ea2867d0caf2c629c5a493d12cb6e94889c7
hassanShakur <hass.shakurhassan@gmail.com>
2023-01-05 14:52:11 +0300

    Cards props.children prop

--- myapp/src/components/Expenses.js
+++ myapp/src/components/Expenses.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import SingleExpense from './SingleExpense';
+import Card from '../UI/Card';
 
 const Expenses = ({data}) => {
   return (
-    <div className='expenses'>
+    <Card className='expenses'>
       <SingleExpense data={data[0]}/>
       <SingleExpense data={data[1]}/>
       <SingleExpense data={data[2]}/>
       <SingleExpense data={data[3]}/>
-    </div>
+    </Card>
   );
 };
 
